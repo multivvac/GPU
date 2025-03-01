51ea8cfb305c097640a9dd8d2c703374af11d76a
multivac <huangweiurob@gmail.com>
2025-03-01 23:51:06 +0100

    chore: change directory, need to think about how to structure the benchmark stuff

--- quizzes/histogram/quiz.cpp
+++ quizzes/histogram/quiz.cpp
@@ -1,3 +1,4 @@
+
 #include "utils/timer.hpp"
 #include <ATen/cuda/CUDAGeneratorImpl.h>
 #include <iostream>
@@ -6,18 +7,18 @@
 int main() {
   auto timer = StopWatch<chrono_alias::ns>();
   timer.start();
-  int size = 1;
+  int size = 3;
   int contention = 10;
   auto gen = torch::make_generator<at::CUDAGeneratorImpl>();
-  auto data = torch::randint(0, 256, {size}, gen,
-                             torch::dtype(torch::kInt8).device(torch::kCUDA));
+  auto data = torch::randint(0, 256, {size, 2}, gen,
+                             torch::dtype(torch::kUInt8).device(torch::kCUDA));
   auto evilvalue = torch::randint(
-      0, 256, {}, gen, torch::dtype(torch::kInt8).device(torch::kCUDA));
+      0, 256, {}, gen, torch::dtype(torch::kUInt8).device(torch::kCUDA));
   auto evilloc =
-      torch::rand({size}, gen,
+      torch::rand({size, 2}, gen,
                   torch::dtype(torch::kFloat32).device(torch::kCUDA)) <
       (contention / 100.0);
-  data[evilloc] = evilvalue;
+  data.masked_fill_(evilloc, evilvalue);
   std::cout << data << std::endl;
   timer.stop();
 
---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include "utils/timer.hpp"
+#include <ATen/cuda/CUDAGeneratorImpl.h>
+#include <iostream>
+#include <torch/torch.h>
+
+int main() {
+  auto timer = StopWatch<chrono_alias::ns>();
+  timer.start();
+  int size = 3;
+  int contention = 10;
+  auto gen = torch::make_generator<at::CUDAGeneratorImpl>();
+  auto data = torch::randint(0, 256, {size, 2}, gen,
+                             torch::dtype(torch::kUInt8).device(torch::kCUDA));
+  auto evilvalue = torch::randint(
+      0, 256, {}, gen, torch::dtype(torch::kUInt8).device(torch::kCUDA));
+  auto evilloc =
+      torch::rand({size, 2}, gen,
+                  torch::dtype(torch::kFloat32).device(torch::kCUDA)) <
+      (contention / 100.0);
+  data.masked_fill_(evilloc, evilvalue);
+  std::cout << data.contiguous() << std::endl;
+  timer.stop();
+
+  std::cout << "duration: " << timer.getTime().count() << std::endl;
+}
